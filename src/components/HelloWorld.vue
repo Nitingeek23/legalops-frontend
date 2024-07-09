e0938724167ed3abe66f6e8b2d6915957b354dbf
Pronay Guha <pronayguha13@gmail.com>
2024-07-09 22:30:14 +0530

    Init setup with shadcn-vue

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Button } from '@/components/custom/button'
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <Button> {{ msg }}</Button>
+</template>
+
+<style scoped></style>
