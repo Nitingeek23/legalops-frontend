35cfae0b3e7e53b4682e864225e3e9fcc1ff103f
Pronay Guha <pronayguha13@gmail.com>
2024-07-13 01:25:45 +0530

    AboutUS

---
+++ src/components/LandingScreen/AboutUs.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="max-h-full flex flex-col gap-8 py-16 px-16">
+    <div class="flex flex-row justify-start items-start">
+      <img
+        src="../../assets/landing-page-car-image.png"
+        alt="client image"
+        class="w-[400px] h-[480px]"
+      />
+      <div class="p-8 text-start flex-grow">
+        <h3 class="font-bold text-[36px] pb-8">Virtual Condition For Hearing</h3>
+        <p class="text-[18px]">
+          CLRMDR creates quality hearing bundles & video for arbitration hearings and delivers them
+          as pan drive soft copy and hard copy. The hearing bundles follow the guidelines set out by
+          top courts across the world. They make referring easy and bring all the participants of a
+          hearing on the same page fast.
+        </p>
+        <p class="pt-8 font-light">
+          Write to arbitration.newdelhi@gmail.com<br />to use the service for your upcoming hearing.
+        </p>
+      </div>
+    </div>
+    <div class="flex flex-row-reverse justify-start items-start">
+      <img
+        src="../../assets/landing-page-car-image.png"
+        alt="client image"
+        class="w-[400px] h-[500px]"
+      />
+      <div class="p-8 text-start flex-grow">
+        <h3 class="font-bold text-[36px] pb-8">Dispute Resolution</h3>
+        <p class="text-[18px]">
+          CLRMDR creCLRMDR offers the most accessible way of resolving disputes with its online
+          arbitration, mediation and conciliation process
+        </p>
+        <ul class="list-inside">
+          <li class="text-[18px] list-disc">
+            Register a single case or thousands of cases at once
+          </li>
+          <li class="text-[18px] list-disc">
+            Appoint suitable arbitrator / mediator from a large expert panel
+          </li>
+          <li class="text-[18px] list-disc">
+            Relieve yourself of all the administrative aspects of the case
+          </li>
+        </ul>
+        <p class="text-[18px]">
+          Become more environment friendly with end-to-end digital processates quality hearing
+          bundles & video for arbitration hearings and delivers them as pan drive soft copy and hard
+          copy. The hearing bundles follow the guidelines set out by top courts across the world.
+          They make referring easy and bring all the participants of a hearing on the same page
+          fast.
+        </p>
+      </div>
+    </div>
+    <div class="flex flex-row justify-start items-start">
+      <img
+        src="../../assets/landing-page-car-image.png"
+        alt="client image"
+        class="w-[400px] h-[500px]"
+      />
+      <div class="p-8 text-start flex-grow">
+        <h3 class="font-bold text-[36px] pb-8">CRLMDR Hearing Management Services</h3>
+        <ul class="list-inside">
+          <li class="text-[18px] list-disc font-normal">
+            Secure hearing rooms with breakout rooms
+          </li>
+          <li class="text-[18px] list-disc font-normal">
+            Facility to make submissions as text, audio, video, documents
+          </li>
+          <li class="text-[18px] list-disc font-normal">
+            Secure Recording and Live-Streaming services
+          </li>
+          <li class="text-[18px] list-disc font-normal">
+            Transcription services for real time and delayed transcripts
+          </li>
+          <li class="text-[18px] list-disc font-normal">
+            Stenographers who can join the hearings on a short notice
+          </li>
+          <li class="text-[18px] list-disc font-normal">Hearing Bundle preparation service</li>
+          <li class="text-[18px] list-disc font-normal">Witness Observer service</li>
+          <li class="text-[18px] list-disc font-normal">Case Manager Assistance</li>
+        </ul>
+      </div>
+    </div>
+    <div class="flex flex-row-reverse justify-start items-start">
+      <img
+        src="../../assets/landing-page-car-image.png"
+        alt="client image"
+        class="w-[400px] h-[500px]"
+      />
+      <div class="p-8 text-start flex-grow">
+        <h3 class="font-bold text-[36px] pb-8">Arbitrator Appointment</h3>
+        <p class="text-[18px] font-normal">
+          Companies dealing with a high volume of disputes find it difficult to access and appoint
+          arbitrators. CLRMDR, as an independent institution with its large panel of arbitrators,
+          acts as appointing authority and enables such organizations to resolve disputes in a
+          legally compliant manner. CLRMDR panel of arbitrators and mediators are also well trained
+          to use the platform and the associated technologies. They also leverage the technology for
+          administrative processes and focus on the dispute and its resolution.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- src/components/LandingScreen/LandingPageBanner.vue
+++ src/components/LandingScreen/LandingPageBanner.vue
@@ -1,12 +1,8 @@
 <template>
   <!-- <ScrollArea class=""> -->
   <ScrollArea class="flex-grow h-full" id="scroll-container">
-    <div class="bg-[#0000ff]" :style="`height: ${heightVar}`">
-      Jokester began sneaking into the castle in the middle of the night and leaving jokes all over
-      the place: under the king's pillow, in his soup, even in the royal toilet. The king was
-      furious, but he couldn't seem to stop Jokester. And then, one day, the people of the kingdom
-      discovered that the jokes left by Jokester were so funny that they couldn't help but laugh.
-      And once they started laughing, they couldn't stop.
+    <div>
+      <AboutUs />
     </div>
     <div class="bg-[#0000ff]" :style="`height: ${heightVar}`">
       Jokester began sneaking into the castle in the middle of the night and leaving jokes all over
@@ -28,6 +24,7 @@
 
 <script setup lang="ts">
 import ScrollArea from '@/components/custom/scroll-area/ScrollArea.vue'
+import AboutUs from '@/components/LandingScreen/AboutUs.vue'
 import { onMounted, ref } from 'vue'
 const heightVar = ref<string>('200px')
 
